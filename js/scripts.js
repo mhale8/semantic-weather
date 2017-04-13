eeed59b22f6947992cc007cfe9a17eef4f124085
mhale8 <meredithhale@Merediths-MacBook-Pro.local>
2017-04-13 16:13:13 -0700

    Added More Features
    - Accordions
    - Simple Weather Plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-// Scripts
+// Get Weather
+$.simpleWeather({
+    location: 99004,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('.image img').attr('src', weather.image );
+      $('.description p').text('src', weather.text );
+      
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
 
-console.log('Hello Humans!');
+// Go Accordian
+$('.ui.accordion')
+  .accordion()
+;
